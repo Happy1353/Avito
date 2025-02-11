42df1658b4418459f7b092a3c98d56e66217476f
Aleksandr Vishnyakov <vishnyakov1353@gmail.com>
2025-02-11 22:06:05 +0300

    add Docker and migrations

---
+++ migrations/000001_init.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE purchases;
+DROP TABLE transactions;
+DROP TABLE merchandise;
+DROP TABLE users;
---
+++ migrations/000001_init.up.sql
@@ -0,0 +1,30 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT UNIQUE NOT NULL,
+    balance INT DEFAULT 1000 NOT NULL
+);
+
+CREATE TABLE merchandise (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    price INT NOT NULL
+);
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    sender_id INT REFERENCES users(id) ON DELETE CASCADE,
+    receiver_id INT REFERENCES users(id) ON DELETE CASCADE,
+    amount INT NOT NULL,
+    created_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE purchases (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    merchandise_id INT REFERENCES merchandise(id) ON DELETE CASCADE,
+    created_at TIMESTAMP DEFAULT now()
+);
+
+INSERT INTO merchandise (name, price) VALUES 
+    ('t-shirt', 80), ('cup', 20), ('book', 50), ('pen', 10), ('powerbank', 200),
+    ('hoody', 300), ('umbrella', 200), ('socks', 10), ('wallet', 50), ('pink-hoody', 500);
